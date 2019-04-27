008ef68c6e92b059b2379d8ea7cfcddf3fd35921
muath93 <mu3athme@gmail.com>
2019-04-27 17:47:48 +0300

    connect to database

--- server.js
+++ server.js
@@ -12,8 +12,13 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
+        // connectionString: process.env.DATABASE_URL,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        port: process.env.DATABASE_PORT,
+        host: process.env.DATABASE_HOST,
+        ssl: true
     }
 });
 
@@ -23,7 +28,7 @@ const db = knex({
 //         host: '127.0.0.1',
 //         user: 'postgres',
 //         password: 'root',
-//         database: 'smart_brain'
+//         database: 'smart-brain'
 //     }
 // });
 
@@ -45,6 +50,26 @@ const PORT = process.env.PORT || '3000';
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT} `);
 })
+
+db.schema.createTable('users', (table) => {
+    table.increments().primary();
+    table.string('name', [100]);
+    table.text('email').unique().notNullable();
+    table.bigInteger('entries').defaultTo(0)
+    table.timestamp('joined');
+})
+    .then(() => {
+        db.destroy();
+    })
+
+db.schema.createTable('login', (table) => {
+    table.increments().primary();
+    table.string('hash', [100]).notNullable();
+    table.text('email').unique().notNullable();
+})
+    .then(() => {
+        db.destroy();
+    })
 
 app.get('/', (req, res) => { res.send('It is working') })
 
